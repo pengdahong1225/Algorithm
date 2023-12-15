685f36c85f62041d104c8c4741ec2a8438373102
pengdahong1225 <2102533303@qq.com>
2023-12-15 11:59:35 +0800

    xxx

--- common/source.h
+++ common/source.h
@@ -8,28 +8,36 @@
 #include<algorithm>
 #include<functional>
 #include<queue>
+
 using namespace std;
+
 /* 链表 */
 typedef struct ListNode {
     ListNode(int x) : val(x), next(nullptr) {}
+
     int val;
-    struct ListNode* next;
-}ListNode;
+    struct ListNode *next;
+} ListNode;
+
 /* 二叉树 */
 typedef struct TreeNode {
     int val;
-    struct TreeNode* left;
-    struct TreeNode* right;
+    struct TreeNode *left;
+    struct TreeNode *right;
+
     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+
     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
-    TreeNode(int x, TreeNode* left, TreeNode* right) : val(x), left(left), right(right) {}
+
+    TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
 };
+
 /* 线索二叉树 */
-typedef struct ThreadTreeNode{
+typedef struct ThreadTreeNode {
     int val;
-    struct ThreadTreeNode* left;
-    struct ThreadTreeNode* right;
+    struct ThreadTreeNode *left;
+    struct ThreadTreeNode *right;
     /*区别本节点的左右指针指向的是孩子(0)还是前驱后继(1)*/
     int ltag;
     int rtag;
-}ThreadTreeNode;
+} ThreadTreeNode;
